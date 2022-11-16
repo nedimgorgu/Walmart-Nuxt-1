f0338b7376b05a65f4de838d69ad0ec4883871e0
nedimgorgu <89708111+nedimgorgu@users.noreply.github.com>
2022-11-16 17:32:01 +0300

    Proje oluşturuldu ve Bootstrap projeye eklendi

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+
+  <div>
+
+
+    <h1 class="text-center">{{name}}</h1>
+
+  </div>
+  
+</template>
+
+<script>
+export default {
+  
+  data() {
+    return {
+      
+      name:'Merhaba'
+
+    }
+  },
+
+
+}
+</script>
